8dcaa2263ab8b5e65223bd258a95ef4451b5961b
Shaheer Rahi <mshaheer.rahi@gmail.com>
2020-04-15 00:03:34 +0500

    zoom

--- requesttest/views.py
+++ requesttest/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
+     if request.method == 'POST':
+          return HttpResponse(request.POST.items())
+
      return render(request,'requesttest/requesttest.html')
