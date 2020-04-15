099cd4a7dd11afa688450d72fd836542c414183c
Shaheer Rahi <mshaheer.rahi@gmail.com>
2020-04-15 21:52:05 +0500

    caseupdate

--- caseseupdates/views.py
+++ caseseupdates/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.db.models import Sum
+from .models import Case
 # Create your views here.
+def index(request):
+    if request.method=='GET':
+        sum_active=Case.objects.aggregate(Sum('active'))
+        return render(request,"caseseupdates/case.html",{'sum':sum_active['active__sum']})
